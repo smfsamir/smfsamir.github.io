2258dcccd95efffaaffd633331157e0e3f2824d1
smfsamir <f.samir@mail.utoronto.ca>
2021-02-20 22:41:16 -0500

    Updated main app

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-            What's up
-        </p>
+      {/* <header className="App-header"> */}
+      <header>
+        <hr/>
+          <h1>S M Farhan Samir</h1>
+            <h3>MSc Alumnus - Department of Computer Science</h3>
+            <p>fsamir@cs.toronto.edu</p>
+        <hr/>
       </header>
     </div>
   );
