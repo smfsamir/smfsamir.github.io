8bcc506fbf60c0672d9b7305d3a5a630becf660c
smfsamir <f.samir@mail.utoronto.ca>
2021-02-20 21:53:38 -0500

    Update app.js

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -6,13 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hello world
         </p>
       </header>
     </div>
   );
-}
+    }
 
 export default App;
