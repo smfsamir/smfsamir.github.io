9860ae4e9e9bff7b7b38a65df8e8e32bd42f8d2b
smfsamir <f.samir@mail.utoronto.ca>
2021-05-09 22:17:36 -0400

    Add current papers to website

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header"> */}
-      <header>
-        <hr/>
-          <h1>S M Farhan Samir</h1>
-            <h3>MSc Alumnus - Department of Computer Science</h3>
-            <p>fsamir@cs.toronto.edu</p>
-        <hr/>
-      </header>
-    </div>
+    <>
+        <div className="App">
+        {/* <header className="App-header"> */}
+        <header>
+            <hr/>
+            <h1>S M Farhan Samir</h1>
+                <h3>PhD Student</h3>
+                <p>sm.farhan.samir@gmail.com</p>
+            <hr/>
+        </header>
+        </div>
+        <div id="papers">
+                <h3>Papers</h3>
+                <ul>
+                    <li>Samir, F., Beekhuizen, B., Stevenson, S. (2021). <a href="papers/samir_et_al_2021_extremeness.pdf">A formidable ability: Detecting adjectival extremeness with distributional semantic models</a>. In <em>Findings of the Association for Computational Linguistics</em>.</li>
+                    <li>Watson, J., Samir, F., Beekhuizen, B., Stevenson, S. (2021). <a href="papers/watson_et_al_2021_generalization.pdf">Coin it up: Generalization of creative constructions in the wild</a>. In <em>Proceedings of the 42nd Annual Meeting of the Cognitive Science Society</em>.</li>
+                    <li>Samir, F., Beekhuizen, B., Stevenson, S. (2020). <a href="papers/watson_et_al_2020_untangling.pdf">Untangling semantic similarity: Modelling lexical experiments with distributional semantic models</a>. In <em>Proceedings of the 41st Annual Meeting of the Cognitive Science Society</em>.</li>
+                </ul>
+        </div>
+    </>
   );
     }
 
