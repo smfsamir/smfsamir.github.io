1a7e6fbdb96f3de81a1bd907449bb1b5ab46ceb1
smfsamir <f.samir@mail.utoronto.ca>
2021-05-11 09:31:53 -0400

    Bold my name in publication list

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -17,9 +17,9 @@ function App() {
         <div id="papers">
                 <h3>Papers</h3>
                 <ul>
-                    <li>Samir, F., Beekhuizen, B., Stevenson, S. (2021). <a href="papers/samir_et_al_2021_extremeness.pdf">A formidable ability: Detecting adjectival extremeness with distributional semantic models</a>. In <em>Findings of the Association for Computational Linguistics</em>.</li>
-                    <li>Watson, J., Samir, F., Beekhuizen, B., Stevenson, S. (2021). <a href="papers/watson_et_al_2021_generalization.pdf">Coin it up: Generalization of creative constructions in the wild</a>. In <em>Proceedings of the 42nd Annual Meeting of the Cognitive Science Society</em>.</li>
-                    <li>Samir, F., Beekhuizen, B., Stevenson, S. (2020). <a href="papers/watson_et_al_2020_untangling.pdf">Untangling semantic similarity: Modelling lexical experiments with distributional semantic models</a>. In <em>Proceedings of the 41st Annual Meeting of the Cognitive Science Society</em>.</li>
+                    <li><b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2021). <a href="papers/samir_et_al_2021_extremeness.pdf">A formidable ability: Detecting adjectival extremeness with distributional semantic models</a>. In <em>Findings of the Association for Computational Linguistics</em>.</li>
+                    <li>Watson, J., <b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2021). <a href="papers/watson_et_al_2021_generalization.pdf">Coin it up: Generalization of creative constructions in the wild</a>. In <em>Proceedings of the 42nd Annual Meeting of the Cognitive Science Society</em>.</li>
+                    <li><b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2020). <a href="papers/watson_et_al_2020_untangling.pdf">Untangling semantic similarity: Modelling lexical experiments with distributional semantic models</a>. In <em>Proceedings of the 41st Annual Meeting of the Cognitive Science Society</em>.</li>
                 </ul>
         </div>
     </>
