e4296a7adbf155725aeee7417513232886c17b47
smfsamir <f.samir@mail.utoronto.ca>
2021-05-11 10:00:48 -0400

    Update css and publication page

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
         <div className="App">
-        {/* <header className="App-header"> */}
         <header>
             <hr/>
             <h1>S M Farhan Samir</h1>
@@ -15,11 +14,12 @@ function App() {
         </header>
         </div>
         <div id="papers">
-                <h3>Papers</h3>
+                <h3>Peer-reviewed papers</h3>
                 <ul>
-                    <li><b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2021). <a href="papers/samir_et_al_2021_extremeness.pdf">A formidable ability: Detecting adjectival extremeness with distributional semantic models</a>. In <em>Findings of the Association for Computational Linguistics</em>.</li>
-                    <li>Watson, J., <b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2021). <a href="papers/watson_et_al_2021_generalization.pdf">Coin it up: Generalization of creative constructions in the wild</a>. In <em>Proceedings of the 42nd Annual Meeting of the Cognitive Science Society</em>.</li>
-                    <li><b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2020). <a href="papers/watson_et_al_2020_untangling.pdf">Untangling semantic similarity: Modelling lexical experiments with distributional semantic models</a>. In <em>Proceedings of the 41st Annual Meeting of the Cognitive Science Society</em>.</li>
+                    {/* TODO: replace with links to S3*/}
+                    <li><b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2021). <a href="papers/samir_et_al_2021_extremeness.pdf">A formidable ability: Detecting adjectival extremeness with distributional semantic models</a>. In <em>Findings of the Association for Computational Linguistics (long paper)</em>.</li>
+                    <li>Watson, J., <b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2021). <a href="papers/watson_et_al_2021_generalization.pdf">Coin it up: Generalization of creative constructions in the wild</a>. <em>CogSci 43 (<b>oral</b>)</em>.</li>
+                    <li><b>Samir, F.</b>, Beekhuizen, B., Stevenson, S. (2020). <a href="papers/watson_et_al_2020_untangling.pdf">Untangling semantic similarity: Modelling lexical experiments with distributional semantic models</a>. In <em>CogSci 42 (poster)</em>.</li>
                 </ul>
         </div>
     </>
